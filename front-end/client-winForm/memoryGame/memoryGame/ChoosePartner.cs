eb89ca9c80b9b799c21d39371fe1509bfd774f35
efratzadok0879 <efrat.zadok@seldatinc.com>
2018-10-11 12:38:52 +0300

    11/10/2018

---
+++ front-end/client-winForm/memoryGame/memoryGame/ChoosePartner.cs
@@ -0,0 +1,135 @@
+﻿using memoryGame.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Windows.Forms;
+
+namespace memoryGame
+{
+    public partial class ChoosePartner : Form
+    {
+
+        List<User> users;
+        string whoChose = "";//the player who invites the partner
+        public ChoosePartner()
+        {
+            users = GetUsersList();
+            InitializeComponent();
+        }
+
+        private void ChoosePartner_Load(object sender, EventArgs e)
+        {
+            dataGridView_partnerList.DataSource = users.Select(c => new { c.UserName, c.Age }).ToList();
+        }
+
+        private void dataGridView_partnerList_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            User partner = users[e.RowIndex];
+            whoChose = GlobalProp.CurrentUser.UserName;
+            //Request via put method to choose partner
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create($@"http://localhost:57034/ChoosePatner/{partner.UserName}");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "PUT";
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                dynamic currentUserName = GlobalProp.CurrentUser.UserName;
+                string currentUserNameString = Newtonsoft.Json.JsonConvert.SerializeObject(currentUserName, Formatting.None);
+                streamWriter.Write(currentUserNameString);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+            //Get response
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            //Read response
+            using (var streamReader = new System.IO.StreamReader(httpResponse.GetResponseStream(), ASCIIEncoding.ASCII))
+            {
+                string result = streamReader.ReadToEnd();
+                //If request succeeded
+                if (result.Contains("true"))
+                {
+                    //Switch the screen to "startGame"
+                    StartGame startGame = new StartGame(partner);
+                    startGame.Show();
+                    Close();
+                }
+                //Print the matching error
+                else MessageBox.Show(result);
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Every 1000 ms.:
+
+            //Update the shown userslist
+            users = GetUsersList();
+            dataGridView_partnerList.DataSource = users.Select(c => new { c.UserName, c.Age }).ToList();
+
+            User user = GetUser(GlobalProp.CurrentUser.UserName);
+
+            //If anyone chose me:
+            if (user.PartnerUserName != null)
+            {
+                GlobalProp.CurrentUser = user;
+                User partner = GetUser(GlobalProp.CurrentUser.PartnerUserName);
+                partner.PartnerUserName = user.UserName;
+                if (whoChose != GlobalProp.CurrentUser.UserName)
+                {
+                    StartGame startGame = new StartGame(partner);
+                    startGame.Show();
+                    Close();
+                }
+            }
+        }
+
+        public List<User> GetUsersList()
+        {
+            //Get request-GetUsers
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(@"http://localhost:57034/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync("GetUsers").Result; 
+            if (response.IsSuccessStatusCode)
+            {
+                var usersJson = response.Content.ReadAsStringAsync().Result;
+                users = JsonConvert.DeserializeObject<List<User>>(usersJson);
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+            return users;
+        }
+
+        public User GetUser(string username)
+        {
+            //Get request-GetCurrentUser
+            User user = new User();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(@"http://localhost:57034/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync($"GetCurrentUser/{username}").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var userJson = response.Content.ReadAsStringAsync().Result;
+                user = JsonConvert.DeserializeObject<User>(userJson);
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+            return user;
+        }
+    }
+
+}
+    
---
+++ front-end/client-winForm/memoryGame/memoryGame/SignIn.cs
@@ -0,0 +1,80 @@
+﻿using memoryGame.Models;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace memoryGame
+{
+    public partial class SignIn : Form
+    {
+        static HttpClient client = new HttpClient();
+        public SignIn()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSignIn_Click(object sender, EventArgs e)
+        {
+            dynamic NewUser;
+            //Try converting age.
+            try
+            {
+                //Validations 
+                int userAge = int.Parse(age.Text);
+                if (userAge < 18 || userAge > 120)
+                {
+                    MessageBox.Show("User age must be between 18 and 120.");
+                    return;
+                }
+                if(name.Text.Length<2||name.Text.Length>10)
+                {
+                    MessageBox.Show("User name must contains 2 - 10 chars.");
+                    return;
+                }
+
+                //Post Request for Login
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(@"http://localhost:57034/Login");
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    NewUser = new User { UserName = name.Text, Age = int.Parse(age.Text) };
+                    string NewUserString = Newtonsoft.Json.JsonConvert.SerializeObject(NewUser, Formatting.None);
+                    streamWriter.Write(NewUserString);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+                //Gettting response
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                //Reading response
+                using (var streamReader = new System.IO.StreamReader(httpResponse.GetResponseStream(), ASCIIEncoding.ASCII))
+                {
+                    string result = streamReader.ReadToEnd();
+                    //If Login succeeded
+                    if (result.Contains("true"))
+                    {
+                        ChoosePartner choosePartner = new ChoosePartner();
+                        choosePartner.Show();
+                        GlobalProp.CurrentUser = NewUser;
+                    }
+                    //Printing the matching error
+                    else MessageBox.Show(result);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Age must be a number!");
+            }
+
+        }
+    }
+}
+
+
+
